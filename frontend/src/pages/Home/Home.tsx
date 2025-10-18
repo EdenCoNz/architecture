59f6f2fde6c3e16ae36100c01db8c754252ef8e6
PhysicalEd <edenco.nz@gmail.com>
2025-10-19 06:58:17 +1300

    Break

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -11,7 +11,7 @@ import HomeIcon from '@mui/icons-material/Home';
 
 function Home() {
   return (
-    <Container maxWidth="lg">
+    <Containeeer maxWidth="lg">
       <Box
         sx={{
           py: 8,
