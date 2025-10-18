ee5d15d724b329bb2f24c96803486ae0bb5d13ea
PhysicalEd <edenco.nz@gmail.com>
2025-10-19 06:50:40 +1300

    Fix

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -11,7 +11,7 @@ import HomeIcon from '@mui/icons-material/Home';
 
 function Home() {
   return (
-    <Containerfee maxWidth="lg">
+    <Container maxWidth="lg">
       <Box
         sx={{
           py: 8,
