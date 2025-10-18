3b81622ac9a15258d80d8baeab7fc1afc524ba32
PhysicalEd <edenco.nz@gmail.com>
2025-10-19 07:35:20 +1300

    trigger

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -11,7 +11,7 @@ import HomeIcon from '@mui/icons-material/Home';
 
 function Home() {
   return (
-    <Containeeeer maxWidth="lg">
+    <Containeeeeer maxWidth="lg">
       <Box
         sx={{
           py: 8,
